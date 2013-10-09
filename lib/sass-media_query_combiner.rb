7b0b40c4bc35537f73b7aff1e6cd6069f9c99812
Aaron Jensen <aaronjensen@gmail.com>
2013-10-08 22:40:41 -0700

    Document regex

--- lib/sass-media_query_combiner.rb
+++ lib/sass-media_query_combiner.rb
@@ -7,7 +7,27 @@ module Sass
       queries = Hash.new { |hash, key| hash[key] = '' }
       pretty = true
 
-      filtered_data = css.gsub(/\n?(?<query>@media[^{]+){(?<body>(?<braces>(?:[^{}]+)|({\g<braces>}))*)}\n?/m) do |match|
+      filtered_data = css.gsub(/
+        \n?                 # Optional newline
+        (?<query>           # The media query parameters, this will be $1
+          @media            #   Start with @media
+          [^{]+             #   One to many characters that are not {, we are guaranteed to have a space
+        )
+        {
+        (?<body>            # The actual body, this will be $2
+          (?<braces>        #   Recursive capture group
+            (?:
+              [^{}]+        #     Anything that is not a brace
+            )
+            |               #     OR
+            (
+              {\g<braces>}  #     Recursively capture things within braces, this allows for balanced braces
+            )
+          )*                # As many of these as we have
+        )
+        }
+        \n?                 # Optional newline
+        /mx) do |match|
         queries[$1] << $2
         pretty &&= /\n$/m === match
         ''
